a3765dc63dcf6946c30e0953d0e436259e3b2243
MariaCamila <mariacmaza7@gmail.com>
2021-11-24 15:57:11 -0500

    primer ejercicio desarrollado

--- taller_covid.py
+++ taller_covid.py
@@ -5,3 +5,10 @@ Created on Wed Nov 24 15:14:56 2021
 @author: Maria Camila
 """
 
+import pandas as pd
+
+url = 'covid_22_noviembre.csv'
+data = pd.read_csv(url)
+
+# Número de casos de Contagiados en el País.
+print(data['ID de caso'].count())
